294d230b5b78a4f3d0d56c945164c646a9659c6e
cnrdh <cnrdh@users.noreply.github.com>
2021-04-30 16:04:58 +0200

    use deno.land

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
-export { ndmapcommand } from "/home/che/@cnrdh/newline/nd-map/command.js";
-export { generatorFactory } from "/home/che/@cnrdh/newline/generator/factory.js";
+export { ndmapcommand } from "https://deno.land/x/newline/nd-map/command.js";
+export { generatorFactory } from "https://deno.land/x/newline/generator/factory.js";
 
 export { parse as parseArgs } from "https://deno.land/std@0.95.0/flags/mod.ts";
 export { resolve } from "https://deno.land/std@0.95.0/path/mod.ts";
